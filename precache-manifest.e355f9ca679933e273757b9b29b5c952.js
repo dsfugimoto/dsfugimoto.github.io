6bb5491d0a06cced2e4195ec4bddcb3f695e73ac
David Fugimoto <dsfugimoto@gmail.com>
2024-10-16 14:57:12 -0300

    Updates

---
+++ precache-manifest.e355f9ca679933e273757b9b29b5c952.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f1338d3d08b8539e392a59fe9f1575",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f60e5d93a4140348b0d",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "3f60e5d93a4140348b0d",
+    "url": "/static/js/main.c0864bbc.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
